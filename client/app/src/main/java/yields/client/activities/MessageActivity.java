5e8dd20d444ed6ba4db49ae65d9bdf53be842263
Justinien Bouron <justinien.bouron@epfl.ch>
2015-10-16 22:19:08 +0200

    Add mock messages to messageActivity.

--- client/app/src/main/java/yields/client/activities/MessageActivity.java
+++ client/app/src/main/java/yields/client/activities/MessageActivity.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import yields.client.R;
 import yields.client.messages.Message;
+import yields.client.messages.TextContent;
 import yields.client.node.Group;
 import yields.client.node.User;
 import yields.client.yieldsapplication.YieldsApplication;
@@ -48,6 +49,17 @@ public class MessageActivity extends AppCompatActivity {
         /* TODO : call the right method to get message from the server and put them in the
             mMessaage deque. */
         /* For now it fills mMessages with  mock messages. */
-
+        TextContent mockContent1 = new TextContent("Mock Message #1");
+        TextContent mockContent2 = new TextContent("Mock Message #2");
+        long mockId1 = 10;
+        long mockId2 = 2;
+        String mockName1 = "Mock name #1";
+        String mockName2 = "Mock name #2";
+        String mockEmail1 = "mock.1@mock.jpp";
+        String mockEmail2 = "mock.2@mock.jpp";
+        User mockUser1 = new User(mockName1, mockId1, mockEmail1);
+        User mockUser2 = new User(mockName2, mockId2, mockEmail2);
+        mMessages.add(new Message(mockUser1, mockContent1));
+        mMessages.add(new Message(mockUser2, mockContent2));
     }
 }
