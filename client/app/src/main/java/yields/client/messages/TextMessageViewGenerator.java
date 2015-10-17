6f35365b04b02c34854d3a90e45f51900cae5654
Nicolas Roussel <n.roussel13@gmail.com>
2015-10-17 13:28:19 +0200

    Add TextMessageView class

--- client/app/src/main/java/yields/client/messages/TextMessageViewGenerator.java
+++ client/app/src/main/java/yields/client/messages/TextMessageViewGenerator.java
@@ -1,5 +1,7 @@
 package yields.client.messages;
 
+import yields.client.node.ClientUser;
+
 public class TextMessageViewGenerator implements MessageViewGenerator {
 
     public TextMessageViewGenerator(){};
