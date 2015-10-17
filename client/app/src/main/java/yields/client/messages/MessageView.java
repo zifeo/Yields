72c8132d3dd36c987fa3da99cf2b59ad0f87fb84
Justinien Bouron <justinien.bouron@epfl.ch>
2015-10-17 16:02:09 +0200

    Redefine MessageView class.

--- client/app/src/main/java/yields/client/messages/MessageView.java
+++ client/app/src/main/java/yields/client/messages/MessageView.java
@@ -3,18 +3,23 @@ package yields.client.messages;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.LinearLayout;
 
-public class MessageView extends View{
+public class MessageView extends LinearLayout{
 
-    public MessageView(Context context) {
+    public MessageView(Context context, Message m) {
         super(context);
     }
 
-    public MessageView(Context context, AttributeSet attrs) {
+    public MessageView(Context context, AttributeSet attrs, Message m) {
         super(context, attrs);
     }
 
-    public MessageView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public MessageView(Context context, AttributeSet attrs, int defStyleAttr, Message m) {
         super(context, attrs, defStyleAttr);
+    }
+
+    private void createMessageView(Message m){
+        // TODO : add elems from the message in 'this'.
     }
 }
