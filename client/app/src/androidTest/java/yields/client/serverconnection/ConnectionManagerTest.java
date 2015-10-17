f81818046fad6b9931fefdf5b2b93697e636dff9
Trofleb <ncasademont@bluewin.ch>
2015-10-17 15:19:41 +0200

    Add stub class for ConnectionManagerTesting

---
+++ client/app/src/androidTest/java/yields/client/serverconnection/ConnectionManagerTest.java
@@ -0,0 +1,18 @@
+package yields.client.serverconnection;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ConnectionManagerTest {
+
+
+    @BeforeClass
+    public void setup(){
+    }
+
+    @Test
+    public void verifyConnected(){
+
+    }
+}
