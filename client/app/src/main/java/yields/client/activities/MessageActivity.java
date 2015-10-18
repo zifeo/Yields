4e88b264201309fc699d2aad2491e6e73a979c97
Justinien Bouron <justinien.bouron@epfl.ch>
2015-10-18 04:34:24 +0200

    Fix issues in ImageContent class, add image resizing if necessary. Add image buttons in MainActivity.xml for sending and selecting an image.

--- client/app/src/main/java/yields/client/activities/MessageActivity.java
+++ client/app/src/main/java/yields/client/activities/MessageActivity.java
@@ -1,15 +1,23 @@
 package yields.client.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Date;
@@ -19,6 +27,8 @@ import java.util.List;
 import yields.client.R;
 import yields.client.id.Id;
 import yields.client.listadapter.ListAdapter;
+import yields.client.messages.Content;
+import yields.client.messages.ImageContent;
 import yields.client.messages.Message;
 import yields.client.messages.TextContent;
 import yields.client.node.ClientUser;
@@ -27,11 +37,13 @@ import yields.client.node.User;
 import yields.client.yieldsapplication.YieldsApplication;
 
 public class MessageActivity extends AppCompatActivity {
-
     private static ClientUser mUser;
     private static Group mGroup;
     private static ArrayList<Message> mMessages;
     private static ListAdapter mAdapter;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Bitmap mImage; // Image taken from the gallery.
+    private boolean mSendImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +56,15 @@ public class MessageActivity extends AppCompatActivity {
         mGroup = YieldsApplication.getGroup();
         mMessages = new ArrayList<>();
         mAdapter = new ListAdapter(YieldsApplication.getApplicationContext(), R.layout.messagelayout, mMessages);
+        mImage = null;
+        mSendImage = false;
 
         ListView lv = (ListView) findViewById(R.id.messageScrollLayout);
         lv.setAdapter(mAdapter);
 
-        //setTitle(mGroup.getName());
+        //setTitle(mGroup.getName()); TODO
 
-        //retrieveGroupMessages();
+        //retrieveGroupMessages();  TODO
     }
 
     /**
@@ -60,12 +74,31 @@ public class MessageActivity extends AppCompatActivity {
         TextView inputField = (TextView) findViewById(R.id.inputMessageField);
         String inputMessage =  inputField.getText().toString();
         inputField.setText("");
-        TextContent content = new TextContent(inputMessage);
-        Message message = new Message("message", 1230, mUser, content, new Date());
+        Content content = null;
+        Log.i("DEBUG", "Entering if");
+        if (!mSendImage){
+            Log.i("DEBUG", "Creating image");
+            mImage = BitmapFactory.decodeResource(getResources(), R.drawable.image_test);
+            content = new ImageContent(mImage, inputMessage);
+            mSendImage = false;
+        }
+        else {
+            content = new TextContent(inputMessage);
+        }
+        Message message = new Message("message", new Id(1230), mUser, content);
                 // TODO : take tight name and right id.
         mMessages.add(message);
        // mUser.sendMessage(mGroup, message); TODO : implement sendMessage for ClientUser.
         mAdapter.notifyDataSetChanged();
+    }
+
+    public void onClickAddImage(View v){
+        mSendImage = true;
+        pickImageFromGallery();
+        Toast toast = new Toast(YieldsApplication.getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setText("Image added to message");
+        toast.show();
     }
 
     /**
@@ -82,5 +115,28 @@ public class MessageActivity extends AppCompatActivity {
      */
     private void retrieveGroupMessages(){
         mMessages = new ArrayList<>(mUser.getGroupMessages(mGroup));
+    }
+
+    private void pickImageFromGallery(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                mImage = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- client/app/src/main/java/yields/client/messages/ImageContent.java
+++ client/app/src/main/java/yields/client/messages/ImageContent.java
@@ -1,10 +1,17 @@
 package yields.client.messages;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import yields.client.yieldsapplication.YieldsApplication;
@@ -14,7 +21,9 @@ public class ImageContent implements Content{
     private String mCaption;
 
     public ImageContent(Bitmap img, String caption){
-        mImage = Bitmap.createBitmap(img);
+        if (img != null) {
+            mImage = Bitmap.createBitmap(img);
+        }
         mCaption = new String(caption);
     }
 
@@ -25,16 +34,36 @@ public class ImageContent implements Content{
 
     @Override
     public View getView() {
+        WindowManager wm = (WindowManager) YieldsApplication.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int screenWidth = size.x;
+        LinearLayout.LayoutParams params =  new LinearLayout.LayoutParams
+                (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         LinearLayout layout = new LinearLayout(YieldsApplication.getApplicationContext());
+        layout.setLayoutParams(params);
         layout.setOrientation(LinearLayout.VERTICAL);
         ImageView img = new ImageView(YieldsApplication.getApplicationContext());
-        img.setImageBitmap(mImage);
+        Log.d("DEBUG", "Width = " + mImage.getWidth());
+        Log.d("DEBUG", "Height = " + mImage.getHeight());
+        Log.d("DEBUG", "max Width = " + screenWidth);
+        if (mImage.getWidth() > (screenWidth - 200)){ // TODO : remove hardcoded value
+            float scalefactor = ((float)(screenWidth - 200)) / mImage.getWidth();
+            Log.d("DEBUG", "Scale factor = " + scalefactor);
+            Log.d("DEBUG", "New width = " +(screenWidth - 200));
+            Log.d("DEBUG", "New height = " +(int) (scalefactor*mImage.getHeight()));
+            img.setImageBitmap(Bitmap.createScaledBitmap(mImage, (screenWidth - 200), (int) (scalefactor*mImage.getHeight()), false));
+        }
+        else {
+            img.setImageBitmap(mImage);
+        }
         TextView caption = new TextView(YieldsApplication.getApplicationContext());
         caption.setText(mCaption);
         caption.setTextColor(Color.BLACK);
         caption.setTextSize(20.f);
         layout.addView(caption);
         layout.addView(img);
-        return img;
+        return layout;
     }
 }
