9c44ca4a881f5296e0867dc59dd4c1e654538f32
DelamareA <arnaud.delamare@epfl.ch>
2015-10-22 16:02:58 +0200

    Rebase commit

--- client/app/src/main/java/yields/client/node/Group.java
+++ client/app/src/main/java/yields/client/node/Group.java
@@ -1,7 +1,5 @@
 package yields.client.node;
 
-import android.graphics.Bitmap;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -10,53 +8,30 @@ import java.util.Objects;
 
 import yields.client.exceptions.NodeException;
 import yields.client.id.Id;
-import yields.client.messages.Message;
 
 public class Group extends Node {
-    private List<Node> mNodes; // for now, just users
-    private Bitmap mImage;
-    private List<Message> mCurrentMessages; // not all the messages the group has ever sent, just
-                                            // the ones that are currently in the listView
+    private List<User> connectedUsers;
 
-
-    /** Constructor for groups
-     *
-     * @param name The name of the group
-     * @param id The id of the group
-     * @param nodes The current nodes of the group
-     * @param image The current image of the group, can be null
-     * @throws NodeException If nodes is null
-     */
-    public Group(String name, Id id, List<Node> nodes, Bitmap image) throws NodeException {
+    public Group(String name, Id id, List<User> connectedUsers){
         super(name, id);
         Objects.requireNonNull(nodes);
         mNodes = new ArrayList<>(nodes);
         mImage = image;
     }
 
-    public Group(String name, Id id, List<Node> nodes) throws NodeException {
-        this(name, id, nodes, null);
+    private void connectUser(User user) throws NodeException {
+        User newUser = new User(user.getName(), user.getId(),
+                user.getEmail(), user.getImg());
+        connectedUsers.add(newUser);
     }
 
-    public void addNode(Node newNode) {
-        // TODO : Check if node is not a message
-
-        mNodes.add(newNode);
-    }
-
-    public void addMessage(Message newMessage) {
-        mCurrentMessages.add(newMessage);
-    }
-
-    public void setImage(Bitmap image){
-        if (image == null){
-            throw new IllegalArgumentException("Null image in setImage");
+    public void appendUsers(Collection<User> users) throws NodeException {
+        for (User user: users ) {
+            connectUser(user);
         }
-
-        mImage = image;
     }
 
-    public List<Node> getUsers() {
-        return Collections.unmodifiableList(mNodes);
+    public List<User> getUsers() {
+        return Collections.unmodifiableList(connectedUsers);
     }
 }
