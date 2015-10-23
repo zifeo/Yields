03d8f4b3d99c99a1f173875cb2f7548792971181
Justinien Bouron <justinien.bouron@epfl.ch>
2015-10-23 14:39:42 +0200

    ClientUser class : add 'throws' to some methods. YieldsClientUser : new singleton implementing the ClientUser abstract class, sendMessage is implemented.

---
+++ client/app/src/main/java/yields/client/yieldsapplication/YieldsClientUser.java
@@ -0,0 +1,81 @@
+package yields.client.yieldsapplication;
+
+import android.graphics.Bitmap;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import yields.client.exceptions.NodeException;
+import yields.client.id.Id;
+import yields.client.messages.Message;
+import yields.client.messages.TextContent;
+import yields.client.node.ClientUser;
+import yields.client.node.Group;
+import yields.client.node.User;
+import yields.client.serverconnection.ConnectionManager;
+import yields.client.serverconnection.Request;
+import yields.client.serverconnection.RequestBuilder;
+import yields.client.serverconnection.ServerChannel;
+import yields.client.serverconnection.YieldEmulatorSocketProvider;
+
+public class YieldsClientUser extends ClientUser{
+    private static YieldsClientUser mInstance;
+    private static YieldEmulatorSocketProvider mSocketProvider;
+    private static ConnectionManager mConnectionManager;
+    private static ServerChannel mServerChannel;
+
+    private YieldsClientUser(String name, Id id, String email, Bitmap img) throws NodeException, IOException {
+        super(name, id, email, img);
+        mSocketProvider = new YieldEmulatorSocketProvider();
+        mConnectionManager = new ConnectionManager(mSocketProvider);
+        mServerChannel = (ServerChannel) mConnectionManager.getCommunicationChannel();
+    }
+
+    public ClientUser getInstance() {
+        Objects.requireNonNull(mInstance, "Singleton YieldsClientUser has not been instanced yet.");
+        return mInstance;
+    }
+
+    public void createInstance(String name, Id id, String email, Bitmap img) throws InstantiationException, IOException {
+        if (mInstance == null){
+            mInstance = new YieldsClientUser(name, id, email, img);
+        }
+        else{
+            throw new InstantiationException("YieldsClientUser is already instanced.");
+        }
+    }
+
+    @Override
+    public void sendMessage(Group group, Message message) throws IOException {
+        if (message.getContent().getType() == "image"){
+            throw new UnsupportedOperationException("Error, sending image messages is not yet supported.");
+        }
+        TextContent content = (TextContent) message.getContent();
+        Request groupMessageReq = RequestBuilder.GroupMessageRequest(this.getId(), group.getId(), "text", content.getText());
+        mServerChannel.sendRequest(groupMessageReq);
+    }
+
+    @Override
+    public List<Message> getGroupMessages(Group group) {
+        return null;
+    }
+
+    @Override
+    public void addNewGroup(Group group) {
+
+    }
+
+    @Override
+    public void deleteGroup(Group group) {
+
+    }
+
+    @Override
+    public Map<User, String> getHistory(Group group, Date from) {
+        return null;
+    }
+}
