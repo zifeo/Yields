f572968872f4efce95cb47218e559cb2b9a9c995
Teo Stocco <teo.stocco@epfl.ch>
2015-10-25 20:04:34 +0100

    update database class registration

--- server/src/main/scala/yields/server/dbi/package.scala
+++ server/src/main/scala/yields/server/dbi/package.scala
@@ -3,9 +3,9 @@ package yields.server
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 
-import com.orientechnologies.common.log.OLogManager
 import com.orientechnologies.orient.`object`.db.OObjectDatabaseTx
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery
+import yields.server.dbi.models.{User, Group}
 import yields.server.utils.{Config, Helpers}
 
 import scala.collection.JavaConverters._
@@ -16,7 +16,8 @@ package object dbi {
   private[dbi] lazy implicit val database: OObjectDatabaseTx = {
     val db = new OObjectDatabaseTx(Config.getString("database.uri"))
     db.open(Config.getString("database.user"), Config.getString("database.pass"))
-    db.getEntityManager.registerEntityClasses("yields.server.dbi.models")
+    db.getEntityManager.registerEntityClass(classOf[User])
+    db.getEntityManager.registerEntityClass(classOf[Group])
     db
   }
 
@@ -25,6 +26,7 @@ package object dbi {
   /** Terminates database connection. */
   def closeDatabase(): Unit = database.close()
 
+  /** Query the database. */
   def queryBySql[T](sql: String, params: AnyRef*)(implicit db: OObjectDatabaseTx): List[T] = {
     val params4java = params.toArray
     val results: java.util.List[T] = db.query(new OSQLSynchQuery[T](sql), params4java: _*)
