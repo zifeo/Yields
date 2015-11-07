22a9a0d768d4e274f1802eff3e967b5e7790ec7f
hottingerj <hottinger.jeremy@gmail.com>
2015-11-07 23:02:14 +0100

    Node model updated with new getter and setters

--- server/src/main/scala/yields/server/dbi/models/Node.scala
+++ server/src/main/scala/yields/server/dbi/models/Node.scala
@@ -1,5 +1,7 @@
 package yields.server.dbi.models
 
+import java.time.OffsetDateTime
+
 import yields.server.dbi._
 import yields.server.utils.Helpers
 
@@ -17,33 +19,60 @@ import yields.server.utils.Helpers
 /**
  * Representation of a node
  */
-class Node(val nid: NID) {
+abstract class Node(val nid: NID) {
 
   object Key {
     val infos = s"nodes:$nid"
-    val users = s"nodes:$nid:users"
-    val nodes = s"nodes:$nid:nodes"
-    val feed = s"nodes:$nid:feed"
-    val tid = s"nodes:$nid:tid"
+    val date_creation = "creation"
+    val last_activity = "activity"
+    val updated_at = "updated_at"
+    val name = "name"
+    val kind = "kind"
+    val users = s"$infos:users"
+    val nodes = s"$infos:nodes"
+    val feed = s"$infos:feed"
+    val tid = s"$infos:tid"
   }
 
   type feedEntry = Map[TID, feedContent]
 
-  var _infos: Option[Map[String, String]] = None
+  var _date_creation: Option[OffsetDateTime] = None
+  var _last_activity: Option[OffsetDateTime] = None
+  var _name: Option[String] = None
+  var _kind: Option[String] = None
+
   var _users: Option[List[UID]] = None
   var _nodes: Option[List[NID]] = None
   var _feed: Option[List[feedEntry]] = None
 
 
-  /** infos getter */
-  def infos: Map[String, String] = _infos.getOrElse {
-    _infos = redis.hmget(Key.infos)
-    valueOrDefault(_infos, Map())
+  /** dateCreation getter */
+  def date_creation: OffsetDateTime = _date_creation.getOrElse {
+    _date_creation = redis.hget(Key.infos, Key.date_creation).map(OffsetDateTime.parse)
+    valueOrException(_date_creation)
   }
 
-  /** infos setter */
-  def infos_(newInfos: Map[String, String]): Unit =
-    _infos = update(Key.infos, newInfos)
+  /** lastActivity getter */
+  def last_activity: OffsetDateTime = _last_activity.getOrElse {
+    _last_activity = redis.hget(Key.infos, Key.last_activity).map(OffsetDateTime.parse)
+    valueOrException(_last_activity)
+  }
+
+  /** name getter */
+  def name: String = _name.getOrElse {
+    _name = redis.hget(Key.infos, Key.name)
+    valueOrException(_name)
+  }
+
+  /** name setter */
+  def name_(n: String): Unit =
+  _name = update(Key.name, n)
+
+  /** kind getter */
+  def kind: String = _kind.getOrElse{
+    _kind = redis.hget(Key.infos, Key.kind)
+    valueOrException(_kind)
+  }
 
   /** users getter */
   def users: List[UID] = _users.getOrElse {
@@ -71,74 +100,17 @@ class Node(val nid: NID) {
   private def valueOrDefault[T](value: Option[T], default: T): T =
     value.getOrElse(default)
 
-  // Updates the field with given value and actualize timestamp.
   private def update[T](field: String, value: T): Option[T] = {
-    val status = redis.hmset(field, List((field, value)))
-    if (!status) throw new RedisNotAvailableException
+    val status = redis.hmset(Key.infos, List((field, value), (Key.updated_at, Helpers.currentDatetime)))
+    if (! status) throw new RedisNotAvailableException
     Some(value)
   }
 
   // Returns whether the count is one or throws an exception on error.
   private def hasChangeOneEntry(count: Option[Long]): Boolean =
     1 == count.getOrElse(throw new RedisNotAvailableException)
-}
 
-object Node {
-
-
-  //  def createGroup(name: String): GID = {
-  //
-  //    val struct = Map(
-  //      "datetime" -> Helpers.currentDatetime.toString,
-  //      "name" -> name
-  //    )
-  //
-  //    val gid = r.incr("ids:last:groups") match {
-  //      case Some(id) => id
-  //      case None => throw new Exception
-  //    }
-  //
-  //    r.hmset(s"groups:$gid", struct)
-  //    r.set(s"groups:$gid:nid", "0")
-  //
-  //    gid
-  //  }
-  //
-  //
-  //    val nid = r.incr(s"groups:$ridGroup:nid") match {
-  //      case Some(id) => id
-  //      case None => throw new Exception
-  //    }
-  //
-  //    val struct = Map(
-  //      "sender" -> ridSender,
-  //      "datetime" -> Helpers.currentDatetime,
-  //      "content" -> body
-  //    )
-  //
-  //    r.hmset(s"groups:$ridGroup:content:$nid", struct)
-  //
-  //    nid
-  //
-  //  }
-  //
-  //  def getLast(ridGroup: String, last: Long, count: Long) = {
-  //
-  //    val lastNid = r.get(s"groups:$ridGroup:nid") match {
-  //      case Some(id) => id
-  //      case None => throw new Exception
-  //    }
-  //
-  //    val from = Math.max(0, lastNid.toInt - count) to lastNid.toInt
-  //
-  //    val result = from.map { nid =>
-  //
-  //      r.hmget(s"groups:$ridGroup:content:$nid", "datetime", "sender", "content")
-  //
-  //    }.flatten
-  //
-  //    println(result)
-  //
-  //  }
-
+  // Gets the value if set or else throws an exception (cannot be unset).
+  private def valueOrException[T](value: Option[T]): T =
+    value.getOrElse(throw new ModelValueNotSetException)
 }
