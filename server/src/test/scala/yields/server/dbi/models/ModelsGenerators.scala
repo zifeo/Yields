9ce4d8c86722e1610c27f5200a0ce28b45dc4f91
Teo Stocco <teo.stocco@epfl.ch>
2015-11-08 01:49:02 +0100

    correct lastest tests in accordance with new models

--- server/src/test/scala/yields/server/dbi/models/ModelsGenerators.scala
+++ server/src/test/scala/yields/server/dbi/models/ModelsGenerators.scala
@@ -1,9 +1,6 @@
 package yields.server.dbi.models
 
-import java.util.Date
-
 import org.scalacheck.Arbitrary
-import yields.server._
 
 trait ModelsGenerators {
 
@@ -11,28 +8,14 @@ trait ModelsGenerators {
 
   implicit lazy val groupArb: Arbitrary[Group] = Arbitrary {
     for {
-      gid <- cleanStringGen
-      name <- cleanStringGen
-      lastActivity <- arbitrary[Date]
-      g = new Group()
-    } yield {
-      g.id = gid
-      g.group_name = name
-      g.refreshed_at = lastActivity
-      g
-    }
+      nid <- arbitrary[NID]
+    } yield Group(nid)
   }
 
   implicit lazy val userArb: Arbitrary[User] = Arbitrary {
     for {
-      uid <- cleanStringGen
-      name <- cleanStringGen
-      u = new User()
-    } yield {
-      u.id = uid
-      u.name = name
-      u
-    }
+      uid <- arbitrary[UID]
+    } yield User(uid)
   }
 
 }
--- server/src/test/scala/yields/server/io/GroupsJsonFormatSpecifications.scala
+++ server/src/test/scala/yields/server/io/GroupsJsonFormatSpecifications.scala
@@ -9,7 +9,7 @@ object GroupsJsonFormatSpecifications extends Properties("GroupsJsonFormat") wit
 
   import Prop.forAll
 
-  property("GroupCreate") = forAll { (x: NodeCreate) =>
+  property("GroupCreate") = forAll { (x: GroupCreate) =>
     toAndFromJson(x) == x
   }
 
