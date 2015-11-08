e7d963ca1fb8ea798a267a91f2a452fb22f8dc06
hottingerj <hottinger.jeremy@gmail.com>
2015-11-09 00:13:27 +0100

    add doc to group and image classes

--- server/src/main/scala/yields/server/dbi/models/Group.scala
+++ server/src/main/scala/yields/server/dbi/models/Group.scala
@@ -3,10 +3,24 @@ package yields.server.dbi.models
 import yields.server.dbi._
 import yields.server.utils.Temporal
 
+/**
+ * Representation of a group
+ * A group is a kind of node dedicated to chat between users
+ *
+ * @param nid Node id to build
+ */
 class Group private (override val nid: NID) extends Node
 
+/**
+ * Companion object for Group
+ */
 object Group {
 
+  /**
+   * Create a new group with the given name
+   * @param name name of the new group
+   * @return the newly created group
+   */
   def createGroup(name: String): Group = {
     val group = Group(Node.newNID())
     redis.withClient { r =>
--- server/src/main/scala/yields/server/dbi/models/Image.scala
+++ server/src/main/scala/yields/server/dbi/models/Image.scala
@@ -8,6 +8,7 @@ import yields.server.dbi.exceptions.{UnincrementalIdentifier, RedisNotAvailableE
  * Represent a image ressource
  * @param nid image id
  *
+ * Special field :
  * nodes:[nid]:content -> Zset path : pathToContent
  *
  */
@@ -19,19 +20,29 @@ class Image private(override val nid: NID) extends Node {
 
   private var _content: Option[String] = None
 
+  /** Image content getter */
   def content: String = _content.getOrElse {
     _content = redis.withClient(_.get[String](Key.content))
     _content.getOrElse(throw new ModelValueNotSetException)
   }
 
+  /** Image content setter */
   def content_=(newPath: String) = {
     redis.withClient(_.set(Key.content, newPath))
   }
 
 }
 
+/**
+ * Companion object for Image
+ */
 object Image {
 
+  /**
+   * Create a new image from a path
+   * @param content path to image
+   * @return the newly created image
+   */
   def createImage(content: String): Image = {
     Image(Node.newNID())
   }
