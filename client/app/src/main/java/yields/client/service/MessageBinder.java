9cb9210f013bc48ff77a5039b8382bb534dd9462
Trofleb <ncasademont@bluewin.ch>
2015-11-08 14:11:45 +0100

    Add a Method to get old message

--- client/app/src/main/java/yields/client/service/MessageBinder.java
+++ client/app/src/main/java/yields/client/service/MessageBinder.java
@@ -3,18 +3,27 @@ package yields.client.service;
 import android.os.Binder;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
+import yields.client.activities.MessageActivity;
 import yields.client.messages.ImageContent;
 import yields.client.messages.Message;
 import yields.client.messages.TextContent;
 import yields.client.node.Group;
 import yields.client.serverconnection.Request;
 import yields.client.serverconnection.RequestBuilder;
+import yields.client.serverconnection.Response;
 
 public class MessageBinder extends Binder {
     private final YieldService mService;
+    private boolean linked;
 
     /**
      * Creates the binder and links it to the service
@@ -22,6 +31,15 @@ public class MessageBinder extends Binder {
      */
     public MessageBinder(YieldService service) {
         mService = service;
+    }
+
+    /**
+     * Binds the activity to the service which permits it to receive incoming messages directly
+     * @param activity the current messagingActivity
+     */
+    public void bind(MessageActivity activity) {
+        Objects.requireNonNull(activity);
+        mService.setMessageActivity(activity);
     }
 
     /**
@@ -34,6 +52,17 @@ public class MessageBinder extends Binder {
         Request groupMessageReq = createRequestForMessageToSend(group, message);
         Log.d("REQUEST", "sendMessage = " + groupMessageReq.message());
         mService.sendRequest(groupMessageReq);
+    }
+
+    public void addMoreGroupMessages(Group group,
+                                 Date lastDate)
+            throws IOException {
+        final int MESSAGE_COUNT = 20;
+
+        Request groupHistoryRequest = RequestBuilder
+                .GroupHistoryRequest(group.getId(), lastDate, MESSAGE_COUNT);
+        mService.sendRequest(groupHistoryRequest);
+        Log.d("REQUEST", "getGroupMessages " + groupHistoryRequest.message());
     }
 
     private static Request createRequestForMessageToSend(Group group, Message message){
@@ -55,5 +84,11 @@ public class MessageBinder extends Binder {
             default : throw new IllegalArgumentException("type unknown");
         }
         return req;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        mService.unsetMessageActivity();
     }
 }
