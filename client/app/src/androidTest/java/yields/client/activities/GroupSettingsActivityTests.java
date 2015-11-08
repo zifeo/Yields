253df7f908722fcf4e095ee5a5acdcccb2589d88
Justinien Bouron <justinien.bouron@epfl.ch>
2015-11-08 16:40:31 +0100

    GroupSettingsActivity : set the right visibility of the group. Add test.

--- client/app/src/androidTest/java/yields/client/activities/GroupSettingsActivityTests.java
+++ client/app/src/androidTest/java/yields/client/activities/GroupSettingsActivityTests.java
@@ -5,6 +5,7 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.junit.Test;
 
@@ -45,7 +46,7 @@ public class GroupSettingsActivityTests extends ActivityInstrumentationTestCase2
         YieldsApplication.setGroup(new Group("Group", new Id(124), new ArrayList<User>()));
     }
 
-    public void testChangeName(){
+    public void testChangeName() throws InterruptedException {
         getActivity();
         onView(withText(R.string.changeGroupName)).perform(click());
         onView(withId(R.id.editText)).perform(typeText(" SWENG"));
@@ -55,9 +56,11 @@ public class GroupSettingsActivityTests extends ActivityInstrumentationTestCase2
 
         onView(withText("Group name changed to \"Group SWENG\"")).inRoot(withDecorView(not(is(getActivity().
                 getWindow().getDecorView())))).check(matches(isDisplayed()));
+        // Wait until the toast disappear.
+        Thread.sleep(2000);
     }
 
-    public void testChangeType(){
+    public void testChangeTypePrivate() throws InterruptedException {
         getActivity();
         onView(withText(R.string.changeGroupType)).perform(click());
         onView(withText(" Private")).perform(click());
@@ -66,14 +69,32 @@ public class GroupSettingsActivityTests extends ActivityInstrumentationTestCase2
 
         onView(withText("Group type changed to : private")).inRoot(withDecorView(not(is(getActivity().
                 getWindow().getDecorView())))).check(matches(isDisplayed()));
+
+        assertEquals(Group.GroupVisibility.PRIVATE, YieldsApplication.getGroup().getVisibility());
+        Thread.sleep(2000);
     }
 
-    public void testAddUsers(){
+    public void testChangeTypePublic() throws InterruptedException {
+        getActivity();
+        onView(withText(R.string.changeGroupType)).perform(click());
+        onView(withText(" Public")).perform(click());
+
+        onView(withText("Ok")).perform(click());
+
+        onView(withText("Group type changed to : public")).inRoot(withDecorView(not(is(getActivity().
+                getWindow().getDecorView())))).check(matches(isDisplayed()));
+
+        assertEquals(Group.GroupVisibility.PUBLIC, YieldsApplication.getGroup().getVisibility());
+        Thread.sleep(2000);
+    }
+
+    public void testAddUsers() throws InterruptedException {
         getActivity();
         onView(withText(R.string.addUsers)).perform(click());
         onView(withId(R.id.actionDoneSelectUser)).perform(click());
 
         onView(withText("0 user(s) added to group")).inRoot(withDecorView(not(is(getActivity().
                 getWindow().getDecorView())))).check(matches(isDisplayed()));
+        Thread.sleep(2000);
     }
 }
