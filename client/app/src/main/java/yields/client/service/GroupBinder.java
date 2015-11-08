95b622a41db67664a7dc6b7706ee5ac52214b8a3
Trofleb <ncasademont@bluewin.ch>
2015-11-08 14:30:55 +0100

    Add the possibility to create a new group from the group binder

--- client/app/src/main/java/yields/client/service/GroupBinder.java
+++ client/app/src/main/java/yields/client/service/GroupBinder.java
@@ -1,6 +1,17 @@
 package yields.client.service;
 
 import android.os.Binder;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import yields.client.id.Id;
+import yields.client.node.Group;
+import yields.client.node.User;
+import yields.client.serverconnection.Request;
+import yields.client.serverconnection.RequestBuilder;
 
 public class GroupBinder extends Binder {
     private final YieldService mService;
@@ -11,5 +22,22 @@ public class GroupBinder extends Binder {
      */
     public GroupBinder(YieldService service) {
         mService = service;
+    }
+
+    /**
+     * Creates a group on the server
+     *
+     * @param group the group to create
+     */
+    public void createNewGroup(Group group) {
+        List<Id> memberIDs = new ArrayList<>();
+        List<User> members = group.getUsers();
+        for (User u : members){
+            memberIDs.add(u.getId());
+        }
+        Request groupAddRequest = RequestBuilder
+                .GroupCreateRequest(group.getUsers().get(0).getId(), group.getName(), memberIDs);
+        Log.d("REQUEST", "Add new group");
+        mService.sendRequest(groupAddRequest);
     }
 }
