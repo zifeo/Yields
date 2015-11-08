69297d4b0e3ae973be723835a1fc99fdfb22f97f
Trofleb <ncasademont@bluewin.ch>
2015-11-08 13:49:07 +0100

    Add groupBinder constructor

--- client/app/src/main/java/yields/client/service/GroupBinder.java
+++ client/app/src/main/java/yields/client/service/GroupBinder.java
@@ -1,4 +1,15 @@
 package yields.client.service;
 
-public class GroupBinder {
+import android.os.Binder;
+
+public class GroupBinder extends Binder {
+    private final YieldService mService;
+
+    /**
+     * Creates the binder and links it to the service
+     * @param service The Service concerned
+     */
+    public GroupBinder(YieldService service) {
+        mService = service;
+    }
 }
