0470d6a953d97835f5068e0540e8b742dc300866
Justinien Bouron <justinien.bouron@epfl.ch>
2015-11-09 14:26:10 +0100

    CreatingAccountActivity.java : Fix and Add javadoc.

--- client/app/src/main/java/yields/client/activities/CreatingAccountActivity.java
+++ client/app/src/main/java/yields/client/activities/CreatingAccountActivity.java
@@ -9,6 +9,10 @@ import yields.client.R;
 public class CreatingAccountActivity extends AppCompatActivity {
     public final static String USERNAME = "yields.client.activities.USERNAME";
 
+    /**
+     * onCreate method for the CreatingAccountActivity.
+     * @param savedInstanceState bundle.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +23,9 @@ public class CreatingAccountActivity extends AppCompatActivity {
         // clientUser.createAccount(savedInstanceState.getString(USERNAME), this);
 
     }
-
-    // Method called by clientUser when the server creates the account
+    /**
+     * Method called by clientUser when the server creates the account
+     */
     public void goToGroupActivity(){
         Intent intent = new Intent(this, GroupActivity.class);
         startActivity(intent);
