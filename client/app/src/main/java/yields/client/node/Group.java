5117c9a7da7a20a34f213a1812d742b200442b1b
Trofleb <ncasademont@bluewin.ch>
2015-11-09 18:51:34 +0100

    Remove unused import

--- client/app/src/main/java/yields/client/node/Group.java
+++ client/app/src/main/java/yields/client/node/Group.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.SortedMap;
 import java.util.TreeMap;
