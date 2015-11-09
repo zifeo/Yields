3c1028d74579bbfbff82ff564f2bc2fb645045e7
Justinien Bouron <justinien.bouron@epfl.ch>
2015-11-09 14:57:21 +0100

    Id.java : Add missing javadoc.

--- client/app/src/main/java/yields/client/id/Id.java
+++ client/app/src/main/java/yields/client/id/Id.java
@@ -9,19 +9,36 @@ public class Id implements IdInterface<String> {
 
     private String mId;
 
+    /**
+     * Constructor for the Id taking a long in parameter.
+     * @param value The id in Long.
+     */
     public Id(long value){
         mId = Long.toString(value);
     }
 
+    /**
+     * Constructor for the id taking a string in parameter.
+     * @param value The id in a string.
+     */
     public Id(String value){
         mId = value;
     }
 
+    /**
+     * Return the value of the id.
+     * @return
+     */
     @Override
     public String getId() {
         return mId;
     }
 
+    /**
+     * Perform equality test between ids.
+     * @param other The id we wnat to compare.
+     * @return True if they are the same, false otherwise.
+     */
     @Override
     public boolean equals(IdInterface<String> other) {
         return Objects.equals(mId, other);
