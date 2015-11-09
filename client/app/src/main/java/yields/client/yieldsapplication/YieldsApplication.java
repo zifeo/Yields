233b7a7115dddb62d9317ee4c7149abda607c92a
Justinien Bouron <justinien.bouron@epfl.ch>
2015-11-09 14:11:22 +0100

    YieldsApplication.java : Add missing javadoc.

--- client/app/src/main/java/yields/client/yieldsapplication/YieldsApplication.java
+++ client/app/src/main/java/yields/client/yieldsapplication/YieldsApplication.java
@@ -21,58 +21,114 @@ public class YieldsApplication {
 
     private static GoogleApiClient mGoogleApiClient;
 
+    /**
+     * Getter for the user of the application.
+     * @return The client user.
+     */
     public static ClientUser getUser(){
         return mUser;
     }
 
+    /**
+     * Getter for the group currently displayed in the MessageActivity.
+     * @return This group.
+     */
     public static Group getGroup(){
         return mGroup;
     }
 
+    /**
+     * Getter for the application context.
+     * @return The context of the application.
+     */
     public static Context getApplicationContext(){
         return mApplicationContext;
     }
 
+    /**
+     * Getter the default group image used on group creation.
+     * @return The default group image.
+     */
     public static Bitmap getDefaultGroupImage(){
         return mDefaultGroupImage;
     }
 
+    /**
+     * Getter the default user image.
+     * @return The default user image.
+     */
     public static Bitmap getDefaultUserImage(){
         return mDefaultUserImage;
     }
 
+    /**
+     * Getter for the google api client of the App.
+     * @return The GoogleAPIClient
+     */
     public static GoogleApiClient getGoogleApiClient(){
         return mGoogleApiClient;
     }
 
+    /**
+     * Setter for the google api client.
+     * @param gac The google api client.
+     */
     public static void setGoogleApiClient(GoogleApiClient gac){
         mGoogleApiClient = gac;
     }
 
+    /**
+     * Setter for the ressources of the app.
+     * @param r The ressources to use.
+     */
     public static void setResources(Resources r){
         mResources = r;
     }
 
+    /**
+     * Setter for the group to be displayed in the Message Activity.
+     * @param g The group.
+     */
     public static void setGroup(Group g){
         mGroup = g;
     }
 
+    /**
+     * Setter for the client user of the app.
+     * @param u The client user to use.
+     */
     public static void setUser(ClientUser u){
         mUser = u;
     }
 
+    /**
+     * Setter for the application context.
+     * @param c The context to use.
+     */
     public static void setApplicationContext(Context c){
         mApplicationContext = c;
     }
 
+    /**
+     * Setter for the group default image.
+     * @param b The default group image to use.
+     */
     public static void setDefaultGroupImage(Bitmap b){
         mDefaultGroupImage = b;
     }
 
+    /**
+     * Setter for the user default image.
+     * @param b The default user image to use.
+     */
     public static void setDefaultUserImage(Bitmap b){
         mDefaultUserImage = b;
     }
 
+    /**
+     * Getter for the ressources of the app.
+     * @return The application ressources.
+     */
     public static Resources getResources(){
         return mResources;
     }
