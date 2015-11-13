ac5b403e5e31ef357b6c2831c2be5cfd793cacc2
Trofleb <ncasademont@bluewin.ch>
2015-11-13 09:32:30 +0100

    Make groupAvtivity tests pass with mock binding

---
+++ client/app/src/androidTest/java/yields/client/generalhelpers/ServiceTestConnection.java
@@ -0,0 +1,18 @@
+package yields.client.generalhelpers;
+
+import yields.client.service.YieldServiceBinder;
+import yields.client.yieldsapplication.YieldsApplication;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+
+public class ServiceTestConnection {
+
+    public static void connectActivityToService(){
+        YieldServiceBinder mockBinder = mock(YieldServiceBinder.class);
+        when(mockBinder.isServerConnected()).thenReturn(true);
+        YieldsApplication.setBinder(mockBinder);
+    }
+}
