355011447b323a9827b29c5e4aeae83ff0245713
Trofleb <ncasademont@bluewin.ch>
2015-11-13 14:50:34 +0100

    Make some automatique parsing for server responses

--- client/app/src/main/java/yields/client/serverconnection/Response.java
+++ client/app/src/main/java/yields/client/serverconnection/Response.java
@@ -9,7 +9,33 @@ import java.util.Objects;
  * The response object which represents a response to be send to the server.
  */
 public class Response{
-    private final JSONObject mRawResponse;
+    /**
+     * The Kind of messages possible
+     */
+    public enum ResKind {
+        GROUPHISTORYRES("GroupHistoryRes");
+        /*PING("PING"), USERCONNECT("UserConnect"), USERUPDATE("UserUpdate"),
+        USERGROUPLIST("UserGroupList"), USERENTOURAGEADD("UserEntourageAdd"),
+        USERENTOURAGEREMOVE("UserEntourageRemove"), USERSTATUS("UserStatus"),
+        GROUPCREATE("GroupCreate"), GROUPUPDATENAME("GroupUpdateName"),
+        GROUPUPDATEVISIBILITY("GroupUpdateVisibility"), GROUPUPDATEIMAGE
+                ("GroupUpdateImage"),
+        GROUPADD("GroupAdd"), GROUPREMOVE("GroupRemove"),
+        GROUPMESSAGE("GroupMessage"), GROUPHISTORY("GroupHistory");*/
+
+        private final String name;
+        ResKind(String name) { this.name = name; }
+        public String getValue() { return name; }
+        public static ResKind getEnumbyName(String name){
+            for(ResKind e : ResKind.values()){
+                if(name == e.name) return e;
+            }
+            return null;
+        }
+    }
+
+    private JSONObject mResponseObj;
+    private ResKind mKind;
 
     /**
      * Creates a response as a Json Object
@@ -19,7 +45,11 @@ public class Response{
      */
     public Response(String rawResponse) throws JSONException {
         Objects.requireNonNull(rawResponse);
-        this.mRawResponse = new JSONObject();
+        this.mResponseObj = new JSONObject(rawResponse);
+        mKind = ResKind.getEnumbyName(mResponseObj.getString("kind"));
+        if(mKind == null) {
+            throw new JSONException("mKind is not known");
+        }
     }
 
     /**
@@ -28,7 +58,7 @@ public class Response{
      * @return The Json Object
      */
     public JSONObject object() {
-        return mRawResponse;
+        return mResponseObj;
     }
 
     /**
@@ -36,6 +66,36 @@ public class Response{
      * @return The response in raw format.
      */
     protected String rawResponse(){
-        return mRawResponse.toString();
+        return mResponseObj.toString();
     }
+
+    /**
+     * Gets the kind of the response
+     *
+     * @return The kind
+     */
+    public ResKind getKind(){
+        return mKind;
+    }
+
+    /**
+     * Gets the Json object of the message from the server response.
+     *
+     * @return The message from the server.
+     * @throws JSONException In case of Json trouble.
+     */
+    public JSONObject getMessage() throws JSONException{
+        return mResponseObj.getJSONObject("message");
+    }
+
+    /**
+     * Gets the Json object of the metadata from the server response.
+     *
+     * @return The metadata from the server response.
+     * @throws JSONException In case of Json trouble.
+     */
+    public JSONObject getMetadata() throws JSONException{
+        return mResponseObj.getJSONObject("metadata");
+    }
+
 }
