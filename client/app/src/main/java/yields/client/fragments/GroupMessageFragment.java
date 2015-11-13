ec3033781d531ace2b708d1782403f38a8ff45ec
Justinien Bouron <justinien.bouron@epfl.ch>
2015-11-13 11:27:30 +0100

    Add class for the fragment responsible of displaying messages of a group.

---
+++ client/app/src/main/java/yields/client/fragments/GroupMessageFragment.java
@@ -0,0 +1,66 @@
+package yields.client.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import yields.client.R;
+import yields.client.listadapter.ListAdapterMessages;
+
+public class GroupMessageFragment extends Fragment{
+    private static View mLayout;
+    private static ListAdapterMessages mAdapter;
+    private static ListView mMessageList;
+    private static AdapterView.OnItemClickListener mListOnClickListener;
+
+    public GroupMessageFragment(){
+
+    }
+
+    public void setAdapter(ListAdapterMessages adapterMessages){
+        mAdapter = adapterMessages;
+    }
+
+    public void setMessageListOnClickListener(AdapterView.OnItemClickListener
+                                                      ocl){
+        mListOnClickListener = ocl;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        Log.d("GroupMessageFragment", "onCreateView");
+        mLayout = inflater.inflate(R.layout
+                .group_message_fragment_layout, container, false);
+        mMessageList = (ListView) mLayout.findViewById(R.id.messagesList);
+        mMessageList.setAdapter(mAdapter);
+        mMessageList.setOnItemClickListener(mListOnClickListener);
+        return mLayout;
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d("GroupMessageFragment", "onStop");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d("GroupMessageFragment", "onPause");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d("GroupMessageFragment", "onResume");
+    }
+
+
+}
