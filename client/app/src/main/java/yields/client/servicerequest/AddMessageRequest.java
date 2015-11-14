2b0cf286d013e8b698a74d9e1aa3e89328620cc1
Nicolas Roussel <n.roussel13@gmail.com>
2015-11-14 18:21:35 +0100

    Add comments and fix attributes' scope.

--- client/app/src/main/java/yields/client/servicerequest/AddMessageRequest.java
+++ client/app/src/main/java/yields/client/servicerequest/AddMessageRequest.java
@@ -13,48 +13,43 @@ import yields.client.serverconnection.RequestBuilder;
 import yields.client.serverconnection.Response;
 import yields.client.yieldsapplication.YieldsApplication;
 
-public class AddMessageRequest extends ServiceRequest{
+public class AddMessageRequest extends ServiceRequest {
 
-    Message mMessage;
-    Group mReceivingGroup;
+    private final Message mMessage;
+    private final Group mReceivingGroup;
 
-    public AddMessageRequest(Message message, Group receivingGroup){
+    /**
+     * Main constructor for this type of ServiceRequest (sending a Message to a Group).
+     *
+     * @param message        The Message that should be sent.
+     * @param receivingGroup The Group to which the Message should be added.
+     */
+    public AddMessageRequest(Message message, Group receivingGroup) {
         Objects.requireNonNull(message);
         Objects.requireNonNull(receivingGroup);
         mMessage = message;
         mReceivingGroup = receivingGroup;
     }
 
+    /**
+     * Returns the type of this ServiceRequest as a String.
+     *
+     * @return The type of this ServiceRequest as a String.
+     */
     @Override
-    public String getType(){
-        return "AddMessage";
-    }
-
-    @Override
-    public Request parseRequestForServer(){
-        return createRequestForMessageToSend(mReceivingGroup,mMessage);
-    }
-
-    @Override
-    public void serviceActionOnResponse(Service service, Response response) {
-
-    }
-
-    public Message getMessage(){
-        return mMessage;
-    }
-
-    public Group getReceivingGroup(){
-        return mReceivingGroup;
+    public String getType() {
+        return MessageKind.GROUPMESSAGE.getValue();
     }
 
     /**
-     * Build a request for sending a message to a group.
-     * @param group The group receiving the message.
-     * @param message The message to be sent to the group.
-     * @return The request.
+     * Build a Request for sending a message to a group.
+     *
+     * @return The Request corresponding to this ServiceRequest.
      */
-    private static Request createRequestForMessageToSend(Group group, Message message){
+    @Override
+    public Request parseRequestForServer() {
+        Group group = getReceivingGroup();
+        Message message = getMessage();
         Objects.requireNonNull(group);
         Objects.requireNonNull(message);
         Request req;
@@ -70,8 +65,32 @@ public class AddMessageRequest extends ServiceRequest{
                                 "text", ((TextContent) message
                                         .getContent()).getText());
                 break;
-            default : throw new IllegalArgumentException("type unknown");
+            default:
+                throw new IllegalArgumentException("Message type not found !");
         }
         return req;
+    }
+
+    @Override
+    public void serviceActionOnResponse(Service service, Response response) {
+        //TODO : @Trofleb
+    }
+
+    /**
+     * Getter method for the Message of this ServiceRequest.
+     *
+     * @return The Message of this ServiceRequest.
+     */
+    public Message getMessage() {
+        return mMessage;
+    }
+
+    /**
+     * Getter method for the receiving Group of this ServiceRequest.
+     *
+     * @return The receiving Group of this ServiceRequest.
+     */
+    public Group getReceivingGroup() {
+        return mReceivingGroup;
     }
 }
--- client/app/src/main/java/yields/client/servicerequest/MessageHistoryRequest.java
+++ client/app/src/main/java/yields/client/servicerequest/MessageHistoryRequest.java
@@ -6,41 +6,68 @@ import java.util.Date;
 
 import yields.client.id.Id;
 import yields.client.serverconnection.Request;
+import yields.client.serverconnection.RequestBuilder;
 import yields.client.serverconnection.Response;
 
-public class MessageHistoryRequest extends ServiceRequest{
+public class MessageHistoryRequest extends ServiceRequest {
 
-    Id mGroupId;
-    Date mFurthestDate;
+    public static final int MESSAGE_COUNT = 10;
 
+    private final Id mGroupId;
+    private final Date mFurthestDate;
 
-    public MessageHistoryRequest(Id groupId, Date furthestDate){
+    /**
+     * Main constructor for this type of ServiceRequest (retrieving Messages from a Group).
+     *
+     * @param groupId      The Id of the Group from which the Messages should be retrieved.
+     * @param furthestDate The furthest Date from which Messages will be retrieved.
+     */
+    public MessageHistoryRequest(Id groupId, Date furthestDate) {
         super();
         mFurthestDate = new Date(furthestDate.getTime());
         mGroupId = groupId;
     }
 
+    /**
+     * Returns the type of this ServiceRequest as a String.
+     *
+     * @return The type of this ServiceRequest as a String.
+     */
     @Override
-    public String getType(){
-        return "GetHistory";
+    public String getType() {
+        return MessageKind.GROUPHISTORY.getValue();
     }
 
+    /**
+     * Build a Request for sending a message to a group.
+     *
+     * @return The Request corresponding to this ServiceRequest.
+     */
     @Override
-    public Request parseRequestForServer(){
-        //TODO request build
-        return null;
+    public Request parseRequestForServer() {
+        return RequestBuilder.GroupHistoryRequest(getGroupId(), getDate(), MESSAGE_COUNT);
     }
 
     @Override
     public void serviceActionOnResponse(Service service, Response response) {
-
+        //TODO : @Trofleb
     }
 
-    public Date getDate(){
+    /**
+     * Getter method for the furthest Date of this ServiceRequest.
+     *
+     * @return The furthest Date of this ServiceRequest.
+     */
+    public Date getDate() {
         return new Date(mFurthestDate.getTime());
     }
 
-    public Id getId(){
+    /**
+     * Getter method for the Id of the Group of this ServiceRequest.
+     *
+     * @return The Id of the Group of this ServiceRequest.
+     */
+    public Id getGroupId() {
         return mGroupId;
     }
 }
