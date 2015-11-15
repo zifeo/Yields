bc80173c3c05970801ba2b6c4e578b107ec66c2f
Teo Stocco <teo.stocco@epfl.ch>
2015-11-15 23:26:12 +0100

    explicit tcp binding parameters

--- server/src/main/scala/yields/server/Yields.scala
+++ server/src/main/scala/yields/server/Yields.scala
@@ -1,18 +1,19 @@
 package yields.server
 
 import akka.actor.ActorSystem
+import akka.io.Tcp.SO
 import akka.stream.scaladsl.Tcp
 import akka.stream.scaladsl.Tcp.IncomingConnection
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings, Supervision}
 import yields.server.pipeline.Pipeline
 import yields.server.utils.Config
 
-import scala.io.StdIn
+import scala.concurrent.duration.Duration
 import scala.util.control.NonFatal
 
 /**
- * Yields server daemon.
- */
+  * Yields server daemon.
+  */
 object Yields extends App {
 
   // Starts system and enable flow errors logging
@@ -28,7 +29,13 @@ object Yields extends App {
   }
 
   // Setups components
-  val connections = Tcp().bind(Config.getString("addr"), Config.getInt("port"))
+  val connections = Tcp().bind(
+    interface = Config.getString("addr"),
+    port = Config.getInt("port"),
+    options = List(SO.KeepAlive(on = false), SO.TcpNoDelay(on = false)),
+    halfClose = false,
+    idleTimeout = Duration.Inf
+  )
   val pipeline = Pipeline()
 
   // Handle connections
