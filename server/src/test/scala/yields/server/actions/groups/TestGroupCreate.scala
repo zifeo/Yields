0f62c4fee4f45a67149f0c1989a5fd4b305851ba
hottingerj <hottinger.jeremy@gmail.com>
2015-11-15 18:13:06 +0100

    TestGroupMessage and TestGroupCreate done

--- server/src/test/scala/yields/server/actions/groups/TestGroupCreate.scala
+++ server/src/test/scala/yields/server/actions/groups/TestGroupCreate.scala
@@ -1,33 +1,51 @@
 package yields.server.actions.groups
 
-import org.scalacheck.{Test, Prop, Properties}
-import yields.server.actions.ActionsGenerators
-import yields.server.dbi.models.{Group, NID, UID}
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+import yields.server.actions.exceptions.ActionArgumentException
+import yields.server.dbi.models.{Node, Group, UID, User}
 import yields.server.mpi.Metadata
-import yields.server.utils.Temporal
+import yields.server.utils.{Config, Temporal}
+
 
 /**
   * Test if GroupCreate action performed well
+  * TODO flush database before and after
   */
-object TestGroupCreate extends Properties("GroupCreate") with ActionsGenerators {
+class TestGroupCreate extends FlatSpec with Matchers with BeforeAndAfter {
 
-  import Prop.forAll
+  /** Switch on test database */
+  /* before {
+    redis.withClient(_.select(Config.getInt("test.database.id")))
+    redis.withClient(_.flushdb)
+  } */
 
-  lazy val m = new Metadata(1, Temporal.current)
+  /** Switch back on main database */
+  /* after {
+    redis.withClient(_.flushdb)
+    redis.withClient(_.select(Config.getInt("database.id")))
+  } */
 
-  val propArgs = forAll { (a: GroupCreate) =>
-    !a.name.isEmpty && !a.users.isEmpty
-  }
+  lazy val m = new Metadata(arbitrary[UID].sample.getOrElse(1), Temporal.current)
+  lazy val users: List[User] = List(User.create("e1"), User.create("e2"), User.create("e3"), User.create("e4"))
+  lazy val nodes: List[Node] = List(Group.createGroup("g1"), Group.createGroup("g2"), Group.createGroup("g3"))
 
-  val propCreateGroup = forAll { (a: GroupCreate) =>
-    val res = a.run(m)
-
+  "running groupCreate action with all parameters set" should "create a group" in {
+    val action = new GroupCreate("GroupName", nodes.map(_.nid), users.map(_.uid))
+    val res = action.run(m)
     res match {
       case GroupCreateRes(nid) =>
-        val group = Group(nid)
-        group.name == a.name && group.users == a.users && group.nodes == a.nodes
-      case _ => false
+        val g = Group(nid)
+        g.users.toSet should be(users.map(_.uid).toSet)
+        g.name should be("GroupName")
+        g.nodes.toSet should be(nodes.map(_.nid).toSet)
     }
   }
+
+  "running groupCreate without name" should "throw an exception" in {
+    val action = new GroupCreate("", nodes.map(_.nid), users.map(_.uid))
+    an[ActionArgumentException] should be thrownBy action.run(m)
+  }
+
 
 }
--- server/src/test/scala/yields/server/actions/groups/TestGroupMessage.scala
+++ server/src/test/scala/yields/server/actions/groups/TestGroupMessage.scala
@@ -1,8 +1,44 @@
 package yields.server.actions.groups
 
-import org.scalacheck.Properties
-import yields.server.actions.ActionsGenerators
+import org.scalacheck.Arbitrary._
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+import yields.server.dbi.models._
+import yields.server.mpi.Metadata
+import yields.server.utils.Temporal
 
-object TestGroupMessage extends Properties("GroupMessage") with ActionsGenerators {
+class TestGroupMessage extends FlatSpec with Matchers with BeforeAndAfter {
+
+  /** Switch on test database */
+  /* before {
+    redis.withClient(_.select(Config.getInt("test.database.id")))
+    redis.withClient(_.flushdb)
+  } */
+
+  /** Switch back on main database */
+  /* after {
+    redis.withClient(_.flushdb)
+    redis.withClient(_.select(Config.getInt("database.id")))
+  } */
+
+  lazy val m = new Metadata(arbitrary[UID].sample.getOrElse(1), Temporal.current)
+  lazy val users: List[User] = List(User.create("e1"), User.create("e2"), User.create("e3"), User.create("e4"))
+  lazy val nodes: List[Node] = List(Group.createGroup("g1"), Group.createGroup("g2"), Group.createGroup("g3"))
+
+  "running groupCreate action with all parameters set" should "create a group" in {
+    val create = new GroupCreate("GroupName", nodes.map(_.nid), users.map(_.uid))
+    val res = create.run(m)
+
+    res match {
+      case GroupCreateRes(nid) =>
+        val action = new GroupMessage(nid, "A message")
+        action.run(m)
+        val g = Group(nid)
+        val msg = g.getMessagesInRange(0, 100)
+        msg.length should be(1)
+        msg.head._4 should be("A message")
+        msg.head._2 should be(m.sender)
+    }
+
+  }
 
 }
