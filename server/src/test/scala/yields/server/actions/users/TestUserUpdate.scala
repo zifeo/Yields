91fbba14a4e083ab17023127fa8da4b775927cd8
hottingerj <hottinger.jeremy@gmail.com>
2015-11-15 22:14:26 +0100

    TestUserUpdate finished

--- server/src/test/scala/yields/server/actions/users/TestUserUpdate.scala
+++ server/src/test/scala/yields/server/actions/users/TestUserUpdate.scala
@@ -1,12 +1,28 @@
 package yields.server.actions.users
 
+import org.scalacheck.Arbitrary._
 import org.scalacheck.Properties
 import org.scalatest.{Matchers, FlatSpec}
 import yields.server.actions.ActionsGenerators
+import yields.server.dbi.models._
+import yields.server.mpi.Metadata
+import yields.server.utils.Temporal
 
 /**
   * Test class for User Update
   */
 class TestUserUpdate extends FlatSpec with Matchers {
+
+  lazy val m = new Metadata(arbitrary[UID].sample.getOrElse(1), Temporal.current)
+
+  "updating all values" should "update everything in database" in {
+    val u = User.create("email@email.com")
+    u.name = "name"
+    val action = new UserUpdate(u.uid, Some("newemail@email.com"), Some("new name"), None)
+    action.run(m)
+    val u2 = User(u.uid)
+    u2.email should be("newemail@email.com")
+    u2.name should be("new name")
+  }
 
 }
