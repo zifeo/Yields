36a72a22cad29c38b85f3b68559fc7deaed93e91
Nicolas Roussel <n.roussel13@gmail.com>
2015-11-18 16:56:46 +0100

    Add MessageStatus enum to Message, and use it as field, tests for this are also added.

--- client/app/src/androidTest/java/yields/client/messages/MessageClassTests.java
+++ client/app/src/androidTest/java/yields/client/messages/MessageClassTests.java
@@ -1,7 +1,6 @@
 package yields.client.messages;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.test.InstrumentationRegistry;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
@@ -17,7 +16,6 @@ import org.junit.Test;
 import java.text.ParseException;
 import java.util.Date;
 
-import yields.client.R;
 import yields.client.activities.MessageActivity;
 import yields.client.activities.MockFactory;
 import yields.client.exceptions.ContentException;
@@ -40,20 +38,74 @@ public class MessageClassTests extends ActivityInstrumentationTestCase2<MessageA
 
     /**
      * Set up for the tests.
-    */
+     */
     @Before
     public void setUp() throws Exception {
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
         YieldsApplication.setApplicationContext(InstrumentationRegistry.getTargetContext());
         YieldsApplication.setResources(getInstrumentation().getContext().getResources());
-        MOCK_TEXT_CONTENT_1  = MockFactory.generateFakeTextContent("Mock text.");
+        MOCK_TEXT_CONTENT_1 = MockFactory.generateFakeTextContent("Mock text.");
+    }
+
+    /**
+     * Tests a Message's getSender() method.
+     */
+    @Test
+    public void testMessageHasCorrectSender() {
+        User user = MockFactory.generateFakeUser("Johnny", new Id(-3), "YOLO@hotmail.jpg");
+        Message message = new Message("message", new Id(-4), user,
+                MockFactory.generateFakeTextContent(1), new Date());
+        assertEquals(user, message.getSender());
+    }
+
+    /**
+     * Tests a Message's getDate() method.
+     */
+    @Test
+    public void testMessageHasCorrectDate() {
+        User user = MockFactory.generateFakeUser("Johnny", new Id(-3), "YOLO@hotmail.jpg");
+        Date date = new Date();
+        Message message = new Message("message", new Id(-4), user,
+                MockFactory.generateFakeTextContent(1), date);
+        assertEquals(0, message.getDate().compareTo(date));
+    }
+
+    /**
+     * Tests a Message's getContent() method.
+     */
+    @Test
+    public void testMessageHasCorrectContent() {
+        User user = MockFactory.generateFakeUser("Johnny", new Id(-3), "YOLO@hotmail.jpg");
+        Content content = MockFactory.generateFakeTextContent(1);
+        Message message = new Message("message", new Id(-4), user, content, new Date());
+        assertEquals(content, message.getContent());
+    }
+
+    /**
+     * Tests a Message's getStatus() method.
+     */
+    @Test
+    public void testMessageHasCorrectStatus() {
+        User user = MockFactory.generateFakeUser("Johnny", new Id(-3), "YOLO@hotmail.jpg");
+        Content content = MockFactory.generateFakeTextContent(1);
+        Message message = new Message("message", new Id(-4), user, content, new Date(),
+                Message.MessageStatus.NOT_SENT);
+        assertEquals(Message.MessageStatus.NOT_SENT, message.getStatus());
+
+        message = new Message("message", new Id(-4), user, content, new Date(),
+                Message.MessageStatus.RECEIVED);
+        assertEquals(Message.MessageStatus.RECEIVED, message.getStatus());
+
+        message = new Message("message", new Id(-4), user, content, new Date(),
+                Message.MessageStatus.SEEN);
+        assertEquals(Message.MessageStatus.SEEN, message.getStatus());
     }
 
     /**
      * Tests a TextContent's getType() method.
      */
     @Test
-    public void testTextContentHasCorrectType(){
+    public void testTextContentHasCorrectType() {
         assertEquals(Content.ContentType.TEXT, MOCK_TEXT_CONTENT_1.getType());
     }
 
@@ -61,8 +113,8 @@ public class MessageClassTests extends ActivityInstrumentationTestCase2<MessageA
      * Tests a ImageContent's getType() method.
      */
     @Test
-    public void testImageContentHasCorrectType(){
-        Bitmap bitmap = BitmapFactory.decodeResource(getActivity().getResources(), R.drawable.send_icon);
+    public void testImageContentHasCorrectType() {
+        Bitmap bitmap = YieldsApplication.getDefaultGroupImage();
         ImageContent imageContent = MockFactory.generateFakeImageContent(bitmap, "Mock caption");
         assertEquals(Content.ContentType.IMAGE, imageContent.getType());
     }
@@ -71,7 +123,7 @@ public class MessageClassTests extends ActivityInstrumentationTestCase2<MessageA
      * Tests a TextContent's getText() method.
      */
     @Test
-    public void testTextContentReturnsCorrectText(){
+    public void testTextContentReturnsCorrectText() {
         assertEquals("Mock text.", MOCK_TEXT_CONTENT_1.getText());
     }
 
@@ -79,8 +131,8 @@ public class MessageClassTests extends ActivityInstrumentationTestCase2<MessageA
      * Test an ImageContent's getCaption() method.
      */
     @Test
-    public void testImageContentReturnsCorrectCaption(){
-        Bitmap bitmap = BitmapFactory.decodeResource(getActivity().getResources(),R.drawable.send_icon);
+    public void testImageContentReturnsCorrectCaption() {
+        Bitmap bitmap = YieldsApplication.getDefaultGroupImage();
         ImageContent imageContent = MockFactory.generateFakeImageContent(bitmap, "Mock caption");
         assertEquals("Mock caption", imageContent.getCaption());
     }
@@ -89,17 +141,17 @@ public class MessageClassTests extends ActivityInstrumentationTestCase2<MessageA
      * Tests an ImageContent's getImage() method.
      */
     @Test
-    public void testImageContentReturnsCorrectImage(){
-        Bitmap bitmap = BitmapFactory.decodeResource(getActivity().getResources(),R.drawable.send_icon);
+    public void testImageContentReturnsCorrectImage() {
+        Bitmap bitmap = YieldsApplication.getDefaultGroupImage();
         ImageContent imageContent = MockFactory.generateFakeImageContent(bitmap, "Mock caption");
-        assertEquals(bitmap, imageContent.getImage());
+        assertTrue(imageContent.getImage().sameAs(bitmap));
     }
 
     /**
      * Tests a TextContent's getView() method.
      */
     @Test
-    public void testTextContentReturnsCorrectLayout(){
+    public void testTextContentReturnsCorrectLayout() {
         View view = MOCK_TEXT_CONTENT_1.getView();
         TextView text = (TextView) view;
         assertEquals("Mock text.", text.getText());
@@ -109,8 +161,8 @@ public class MessageClassTests extends ActivityInstrumentationTestCase2<MessageA
      * Tests an ImageContent's getView() method.
      */
     @Test
-    public void testImageContentReturnsCorrectLayout(){
-        Bitmap bitmap = BitmapFactory.decodeResource(getActivity().getResources(),R.drawable.send_icon);
+    public void testImageContentReturnsCorrectLayout() {
+        Bitmap bitmap = YieldsApplication.getDefaultGroupImage();
         ImageContent imageContent = MockFactory.generateFakeImageContent(bitmap, "Mock caption");
         LinearLayout view = null;
         try {
@@ -126,32 +178,26 @@ public class MessageClassTests extends ActivityInstrumentationTestCase2<MessageA
         //TODO : find a way to check that layout image is correct
     }
 
-    @Test
-    public void testMessagesFromJSONAreCorrectlyParsedForSender() throws JSONException {
-        Message m = new Message(new JSONObject(JSON_MESSAGE));
-        User u = m.getSender();
-        assertEquals((new Id("117")).getId(), u.getId().getId());
-    }
-
-    @Test
-    public void testMessagesFromJSONAreCorrectlyParserForDate() throws JSONException, ParseException {
-        Message m = new Message(new JSONObject(JSON_MESSAGE));
-        Date date = m.getDate();
-        assertEquals(DateSerialization.toDate("2011-12-03T10:15:30+01:00").toString(), date.toString());
-    }
-
-    @Test
-    public void testMessagesFromJSONAreCorrectlyParserForContent() throws JSONException {
-        Message m = new Message(new JSONObject(JSON_MESSAGE));
-        TextContent content = (TextContent) m.getContent();
-        assertEquals("MESSAGE_TEXT", content.getText());
-    }
-
     /**
-     * Tests if a MessageView is buil
+     * Tests Message's constructor that takes a String (JSON format).
      */
     @Test
-    public void testMessageViewReturnsCorrectMessage(){
-      //TODO : find a way to inflate xml from junit test
+    public void testMessagesFromJSONAreCorrectlyParseD() {
+        Message m = null;
+        try {
+            m = new Message(new JSONObject(JSON_MESSAGE));
+        } catch (JSONException e) {
+            fail("Couldn't parse JSON into Message");
+        }
+        User u = m.getSender();
+        assertEquals((new Id("117")).getId(), u.getId().getId());
+        Date date = m.getDate();
+        try {
+            assertEquals(DateSerialization.toDate("2011-12-03T10:15:30+01:00").toString(), date.toString());
+        } catch (ParseException e) {
+            fail("Couldn't deserialize Message's Date");
+        }
+        TextContent content = (TextContent) m.getContent();
+        assertEquals("MESSAGE_TEXT", content.getText());
     }
 }
