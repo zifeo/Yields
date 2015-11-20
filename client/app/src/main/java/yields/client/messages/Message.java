c6d5c3c6ab1f70c8d9d1a7ab4033dda448c1fd94
Nicolas Roussel <n.roussel13@gmail.com>
2015-11-20 15:21:26 +0100

    Add timezone dependancy for dates in the cache

--- client/app/src/main/java/yields/client/messages/Message.java
+++ client/app/src/main/java/yields/client/messages/Message.java
@@ -89,7 +89,7 @@ public class Message extends Node {
      */
     public Message(JSONArray object ) throws JSONException, ParseException{
         super(object.getString(0),
-                new Id(DateSerialization.toDate(object.getString(0)).getTime()));
+                new Id(DateSerialization.dateSerializer.toDate(object.getString(0)).getTime()));
 
         Id idUser = new Id(object.getString(1));
         /* TODO : For now the sender has its id as a name, we need to implement a request to do the mapping. */
@@ -104,7 +104,7 @@ public class Message extends Node {
         //TODO : Implement Groups
 
         try {
-            this.mDate = DateSerialization.toDate(object.getString(0));
+            this.mDate = DateSerialization.dateSerializer.toDate(object.getString(0));
         } catch (ParseException e) {
             throw new JSONException(e.getMessage());
         }
