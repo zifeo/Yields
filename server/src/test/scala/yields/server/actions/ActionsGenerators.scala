ae695cedbe4e6e2edbb8faa462e59039fb935d02
Teo Stocco <teo.stocco@epfl.ch>
2015-11-21 11:49:37 +0100

    add new actions/res to general generators

--- server/src/test/scala/yields/server/actions/ActionsGenerators.scala
+++ server/src/test/scala/yields/server/actions/ActionsGenerators.scala
@@ -7,15 +7,19 @@ trait ActionsGenerators extends GroupsGenerators with UsersGenerators {
   import Gen.oneOf
 
   implicit lazy val actionArb: Arbitrary[Action] = Arbitrary {
-    val arbs = Seq(groupCreateArb, groupUpdateArb, groupMessageArb, groupHistoryArb, userConnectArb, userUpdateArb,
-      userGroupListArb)
+    val arbs = Seq(
+      groupCreateArb, groupUpdateArb, groupMessageArb, groupHistoryArb, groupSearchArb,
+      userConnectArb, userUpdateArb, userGroupListArb, userInfoArb)
+    assert(arbs.size >= 2)
     val gens = arbs.map(_.arbitrary)
     oneOf(gens.head, gens.tail.head, gens.drop(2): _*) // requires at least 2 brute entries
   }
 
   implicit lazy val resultArb: Arbitrary[Result] = Arbitrary {
-    val arbs = Seq(groupCreateResArb, groupUpdateResArb, groupMessageResArb, groupHistoryResArb, userConnectResArb,
-      userUpdateResArb, userGroupListResArb)
+    val arbs = Seq(
+      groupCreateResArb, groupUpdateResArb, groupMessageResArb, groupHistoryResArb, groupSearchResArb,
+      userConnectResArb, userUpdateResArb, userGroupListResArb, userInfoResArb)
+    assert(arbs.size >= 2)
     val gens = arbs.map(_.arbitrary)
     oneOf(gens.head, gens.tail.head, gens.drop(2): _*) // requires at least 2 brute entries
   }
