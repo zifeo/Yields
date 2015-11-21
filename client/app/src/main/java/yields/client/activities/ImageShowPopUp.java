121ee5ada27ddb0b9168df080a831c300d919a7f
Justinien Bouron <justinien.bouron@epfl.ch>
2015-11-21 21:04:50 +0100

    ImageShowPopUp : Define the activity.

--- client/app/src/main/java/yields/client/activities/ImageShowPopUp.java
+++ client/app/src/main/java/yields/client/activities/ImageShowPopUp.java
@@ -1,39 +1,40 @@
 package yields.client.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import yields.client.R;
+import yields.client.yieldsapplication.YieldsApplication;
 
-public class ImageShowPopUp extends AppCompatActivity {
+/**
+ * Class used to display an full image.
+ */
+public class ImageShowPopUp extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_show_pop_up);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_image_show_pop_up, menu);
-        return true;
-    }
+        Bitmap image = YieldsApplication.getShowImage();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        WindowManager wm = (WindowManager) YieldsApplication.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int screenWidth = size.x;
+        float scalefactor = ((float) (screenWidth)) / image.getWidth();
+        Bitmap resizedImage = Bitmap.createScaledBitmap(image, screenWidth, (int)
+                (scalefactor * image.getHeight()), false);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        ImageView imageView = (ImageView) findViewById(R.id.popupimage);
+        imageView.setImageBitmap(resizedImage);
     }
 }
