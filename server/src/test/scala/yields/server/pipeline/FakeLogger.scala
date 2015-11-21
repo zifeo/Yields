8726e811dd061b9755a5d12a008a907cf0ef9a9e
Teo Stocco <teo.stocco@epfl.ch>
2015-11-21 16:15:07 +0100

    factor out akka-stream testing utilities and fake logger class

---
+++ server/src/test/scala/yields/server/pipeline/FakeLogger.scala
@@ -0,0 +1,38 @@
+package yields.server.pipeline
+
+import akka.event.LoggingAdapter
+
+import scala.collection.mutable
+
+/**
+  * Fakes a logger and allows access to all logs.
+  * The log are write-wise thread-safe but the read should be done sequentially.
+  * For testing-purpose only as not fine-grained.
+  */
+final class FakeLogger extends LoggingAdapter {
+
+  val debug = mutable.ListBuffer.empty[String]
+  val info = mutable.ListBuffer.empty[String]
+  val warning = mutable.ListBuffer.empty[String]
+  val error = mutable.ListBuffer.empty[String]
+
+  override def isDebugEnabled: Boolean = true
+  override def isInfoEnabled: Boolean = true
+  override def isWarningEnabled: Boolean = true
+  override def isErrorEnabled: Boolean = true
+
+  override protected def notifyDebug(message: String): Unit = debug.synchronized(debug += message)
+  override protected def notifyInfo(message: String): Unit = info.synchronized(info += message)
+  override protected def notifyWarning(message: String): Unit = warning.synchronized(warning += message)
+  override protected def notifyError(message: String): Unit = error.synchronized(error += message)
+  override protected def notifyError(cause: Throwable, message: String): Unit = error.synchronized(error += message)
+
+  /** Trashes all logs. */
+  def clear(): Unit = {
+    debug.clear()
+    info.clear()
+    warning.clear()
+    error.clear()
+  }
+
+}
--- server/src/test/scala/yields/server/pipeline/package.scala
+++ server/src/test/scala/yields/server/pipeline/package.scala
@@ -2,10 +2,34 @@ package yields.server
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import org.scalacheck.Gen
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 package object pipeline {
 
   implicit lazy val system = ActorSystem("Yields-server-test")
   implicit lazy val materializer = ActorMaterializer()
+
+  /**
+    * Generates elements given the generator and creates a source from it.
+    * FIXME 'scala.collection.immutable.Seq' is to avoid type confusion.
+    * @param gen element generator
+    * @param num number of generated elements, by default 20 (be careful on test timeout if too many)
+    * @tparam T type of element
+    * @return pair of a source and a sequence of the same elements
+    */
+  def generateSource[T](gen: Gen[T], num: Int = 20): (Source[T, Unit], scala.collection.immutable.Seq[T]) = {
+    val generated = (0 until num).flatMap(_ => gen.sample)
+    (Source(generated), generated)
+  }
+
+  /** Blocks 1 second until result is available. */
+  def await[T](run: Future[T]): T = {
+    Await.result(run, 1 second)
+  }
 
 }
