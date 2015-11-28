de387b8c29bded049a69d192c3789e3ea8bea45b
hottingerj <hottinger.jeremy@gmail.com>
2015-11-28 12:50:58 +0100

    complete rss and publisher models

--- server/src/main/scala/yields/server/actions/rss/RSSMessage.scala
+++ server/src/main/scala/yields/server/actions/rss/RSSMessage.scala
@@ -1,5 +1,16 @@
 package yields.server.actions.rss
 
-class RSSMessage {
+import yields.server.actions.{Result, Action}
+import yields.server.mpi.Metadata
 
+/**
+  * Broadcast a rss message
+  */
+case class RSSMessage() extends Action {
+  /**
+    * Run the action given the sender.
+    * @param metadata action requester
+    * @return action result
+    */
+  override def run(metadata: Metadata): Result = ???
 }
--- server/src/main/scala/yields/server/dbi/models/Publisher.scala
+++ server/src/main/scala/yields/server/dbi/models/Publisher.scala
@@ -1,5 +1,8 @@
 package yields.server.dbi.models
 
+import yields.server.dbi._
+import yields.server.utils.Temporal
+
 case class Publisher private(override val nid: NID) extends AbstractPublisher {
 
 }
@@ -7,10 +10,20 @@ case class Publisher private(override val nid: NID) extends AbstractPublisher {
 object Publisher {
 
   def createPublisher(name: String): Publisher = {
-    Publisher(Node.newNID())
+    val publisher = Publisher(Node.newNID())
+    redis.withClient { r =>
+      import publisher.NodeKey
+      val infos = List(
+        (NodeKey.created_at, Temporal.now),
+        (NodeKey.name, name),
+        (NodeKey.kind, classOf[Group].getSimpleName)
+      )
+      r.hmset(publisher.NodeKey.node, infos)
+    }
+    publisher
   }
 
-  def apply(nid: NID) = {
+  def apply(nid: NID): Publisher = {
     new Publisher(nid)
   }
 
