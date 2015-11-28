1d08daba7c09b3814c3051665515acbfff196d91
hottingerj <hottinger.jeremy@gmail.com>
2015-11-28 10:28:28 +0100

    implement PublisherInfo

--- server/src/main/scala/yields/server/actions/publisher/PublisherInfo.scala
+++ server/src/main/scala/yields/server/actions/publisher/PublisherInfo.scala
@@ -1,5 +1,29 @@
 package yields.server.actions.publisher
 
-class PublisherInfo {
+import yields.server.actions.exceptions.UnauthorizeActionException
+import yields.server.actions.{Result, Action}
+import yields.server.dbi.models.{Publisher, User, UID, NID}
+import yields.server.mpi.Metadata
 
+/**
+  * Get infos about a publisher
+  * @param nid nid of the publisher
+  */
+case class PublisherInfo(nid: NID) extends Action {
+  /**
+    * Run the action given the sender.
+    * @param metadata action requester
+    * @return action result
+    */
+  override def run(metadata: Metadata): Result = {
+    val sender = User(metadata.client)
+    if (sender.groups.contains(nid)) {
+      val publisher = Publisher(nid)
+      PublisherInfoRes(nid, publisher.name, None, publisher.users, publisher.nodes)
+    } else {
+      throw new UnauthorizeActionException("client who wants the infos must be a subscriber")
+    }
+  }
 }
+
+case class PublisherInfoRes(nid: NID, name: String, pic: Option[Array[Byte]], users: Seq[UID], nodes: Seq[NID]) extends Result
