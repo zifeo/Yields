d042a0ff76b1aeb35fe882c1c269ff8ccda4a7f3
hottingerj <hottinger.jeremy@gmail.com>
2015-11-29 13:10:46 +0100

    rebase master

--- server/src/main/scala/yields/server/actions/publisher/PublisherMessage.scala
+++ server/src/main/scala/yields/server/actions/publisher/PublisherMessage.scala
@@ -3,8 +3,7 @@ package yields.server.actions.publisher
 import java.time.OffsetDateTime
 
 import yields.server.Yields
-import yields.server.actions.exceptions.{UnauthorizeActionException, ActionArgumentException}
-import yields.server.actions.nodes.NodeMessageRes
+import yields.server.actions.exceptions.{UnauthorizedActionException, ActionArgumentException}
 import yields.server.actions.{Result, Action}
 import yields.server.dbi.models.Media.createMedia
 import yields.server.dbi.models._
@@ -28,22 +27,20 @@ case class PublisherMessage(nid: NID, text: Option[String], contentType: Option[
   override def run(metadata: Metadata): Result = {
     val publisher = Publisher(nid)
     val datetime = Temporal.now
-    if (publisher.users.contains(metadata.client)) {
+    if (!publisher.users.contains(metadata.client))
+      throw new UnauthorizedActionException("only registered user can publish in a publisher")
 
-      val media = for {
-        cntType <- contentType
-        cnt <- content
-      } yield createMedia(cntType, cnt, metadata.client)
+    val media = for {
+      cntType <- contentType
+      cnt <- content
+    } yield createMedia(cntType, cnt, metadata.client)
 
-      media match {
-        case Some(x) => publisher.addMessage((datetime, metadata.client, Some(x.nid), text.getOrElse("")))
-        case None => publisher.addMessage((datetime, metadata.client, None, text.getOrElse("")))
-      }
-
-      PublisherMessageRes(nid, datetime)
-    } else {
-      throw new UnauthorizeActionException("only registered user can publish in a publisher")
+    media match {
+      case Some(x) => publisher.addMessage((datetime, metadata.client, Some(x.nid), text.getOrElse("")))
+      case None => publisher.addMessage((datetime, metadata.client, None, text.getOrElse("")))
     }
+
+    PublisherMessageRes(nid, datetime)
   }
 
 }
