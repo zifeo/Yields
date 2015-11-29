ff01869f19093035dab6680ef77e3d97291e7423
Nicolas Roussel <n.roussel13@gmail.com>
2015-11-29 19:31:14 +0100

    Add saving to Gallery Dialog

--- client/app/src/main/java/yields/client/activities/ImageShowPopUp.java
+++ client/app/src/main/java/yields/client/activities/ImageShowPopUp.java
@@ -1,21 +1,30 @@
 package yields.client.activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
 import yields.client.R;
+import yields.client.fragments.SaveImageToGalleryFragment;
 import yields.client.yieldsapplication.YieldsApplication;
 
 /**
  * Class used to display an full image.
  */
 public class ImageShowPopUp extends Activity {
+
+    private FragmentManager mFragmentManager;
 
     /**
      * Overriden method called on creation.
@@ -40,5 +49,17 @@ public class ImageShowPopUp extends Activity {
 
         ImageView imageView = (ImageView) findViewById(R.id.popupimage);
         imageView.setImageBitmap(resizedImage);
+        mFragmentManager = this.getFragmentManager();
+
+        imageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                SaveImageToGalleryFragment fragment = new SaveImageToGalleryFragment();
+                fragment.show(mFragmentManager, "Save Image to Gallery");
+                return true;
+            }
+        });
     }
+
+
 }
