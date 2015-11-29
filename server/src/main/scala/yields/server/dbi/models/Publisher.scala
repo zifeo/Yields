114aff54d0b456703b1214914ea87338ba700801
hottingerj <hottinger.jeremy@gmail.com>
2015-11-29 15:31:32 +0100

    test some actions on publisher;factor disk access;add pic to users and nodes

--- server/src/main/scala/yields/server/dbi/models/Publisher.scala
+++ server/src/main/scala/yields/server/dbi/models/Publisher.scala
@@ -4,22 +4,24 @@ import yields.server.dbi._
 import yields.server.dbi.models.Node.StaticNodeKey
 import yields.server.utils.Temporal
 
-case class Publisher private(override val nid: NID) extends AbstractPublisher(nid) {
+final class Publisher private(nid: NID) extends AbstractPublisher(nid) {
 }
 
 object Publisher {
 
-  def createPublisher(name: String): Publisher = {
+  def createPublisher(name: String, creator: UID): Publisher = {
     val publisher = Publisher(newIdentity())
 
     redis.withClient { r =>
       val infos = List(
         (StaticNodeKey.created_at, Temporal.now),
         (StaticNodeKey.name, name),
-        (StaticNodeKey.kind, classOf[Group].getSimpleName)
+        (StaticNodeKey.kind, classOf[Group].getSimpleName),
+        (StaticNodeKey.creator, creator)
       )
       r.hmset(publisher.NodeKey.node, infos)
     }
+    publisher.addUser(creator)
     publisher
   }
 
