3aafe4452460834873dc244660342d7b09e4bf27
Teo Stocco <teo.stocco@epfl.ch>
2015-11-29 16:33:13 +0100

    improve router control flow and fault tolerance with logging

--- server/src/test/scala/yields/server/YieldsTests.scala
+++ server/src/test/scala/yields/server/YieldsTests.scala
@@ -26,7 +26,7 @@ class YieldsTests extends DBFlatSpec with Matchers with BeforeAndAfterAll with M
   override def beforeAll(): Unit = {
     logger.info("Starting local server")
     server.start()
-    Thread.sleep(5000) // ensure server is active and bounded before networking with it
+    Thread.sleep(4000) // ensure server is active and bounded before networking with it
   }
 
   override def afterAll(): Unit = {
@@ -76,6 +76,7 @@ class YieldsTests extends DBFlatSpec with Matchers with BeforeAndAfterAll with M
     val client = new FakeClient(1)
     client.send(UserConnect("client@yields.im"))
     await(client.receive()).result should be (UserConnectRes(1, returning = false))
+    client.close()
 
   }
 
@@ -101,6 +102,36 @@ class YieldsTests extends DBFlatSpec with Matchers with BeforeAndAfterAll with M
     val refMessage = clientB.send(GroupMessage(3, Some("hello"), None, None))
     await(clientA.listen()).metadata.ref should be (refMessage)
     await(clientB.receive()).metadata.ref should be (refMessage)
+
+    clientA.close()
+    clientB.close()
+
+  }
+
+  "The system" should "not generate error caused too many requests" in {
+
+    val client = new FakeClient(1)
+    val tries = 100
+
+    client.send(UserConnect("client@yields.im"))
+    await(client.receive()).result should be (UserConnectRes(client.uid, returning = false))
+
+    client.send(GroupCreate("clients", List.empty, List.empty))
+    await(client.receive()).result should be (GroupCreateRes(2))
+
+    for (_ <- 0 to tries) {
+      client.send(GroupMessage(2, Some("hello"), None, None))
+    }
+
+    for (_ <- 0 to tries) {
+      await(client.receive())
+    }
+
+    val sent = client.requests.map(_.metadata.ref)
+    val received = client.responses.map(_.metadata.ref)
+    sent should contain theSameElementsAs received
+
+    client.close()
 
   }
 
