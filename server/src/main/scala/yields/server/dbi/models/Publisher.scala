b5dda334585f92b3b9880ee6a1d80e7c896b0dec
Teo Stocco <teo.stocco@epfl.ch>
2015-12-01 19:33:49 +0100

    uniformize models

--- server/src/main/scala/yields/server/dbi/models/Publisher.scala
+++ server/src/main/scala/yields/server/dbi/models/Publisher.scala
@@ -4,20 +4,35 @@ import yields.server.dbi._
 import yields.server.dbi.models.Node.StaticNodeKey
 import yields.server.utils.Temporal
 
-final class Publisher private(nid: NID) extends AbstractPublisher(nid) {
-}
+/**
+  * Representation of a publisher.
+  * A group is a kind of node dedicated to chat between users with eventual influx from nodes.
+  * It is always public.
+  *
+  * @param nid Node id to build
+  */
+final class Publisher private(nid: NID) extends AbstractPublisher(nid) {}
 
+/** [[Publisher]] companion object. */
 object Publisher {
 
+  /**
+    * Create a new publisher with the given name.
+    * @param name name of the new publisher
+    * @param creator publisher creator
+    * @return the newly created publisher
+    */
   def create(name: String, creator: UID): Publisher = {
     val publisher = Publisher(newIdentity())
-
     redis.withClient { r =>
+      val now = Temporal.now
       val infos = List(
-        (StaticNodeKey.created_at, Temporal.now),
         (StaticNodeKey.name, name),
         (StaticNodeKey.kind, classOf[Group].getSimpleName),
-        (StaticNodeKey.creator, creator)
+        (StaticNodeKey.creator, creator),
+        (StaticNodeKey.created_at, now),
+        (StaticNodeKey.refreshed_at, now),
+        (StaticNodeKey.updated_at, now)
       )
       r.hmset(publisher.NodeKey.node, infos)
     }
@@ -25,6 +40,7 @@ object Publisher {
     publisher
   }
 
+  /** [[Publisher]] constructor. */
   def apply(nid: NID): Publisher = {
     new Publisher(nid)
   }
