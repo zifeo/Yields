5ac561a785dc84f5a300fb4779b2b7202ac5023c
Nicolas Roussel <n.roussel13@gmail.com>
2015-12-01 01:14:45 +0100

    Add activity to add User to Entourage

--- client/app/src/main/java/yields/client/service/RequestHandler.java
+++ client/app/src/main/java/yields/client/service/RequestHandler.java
@@ -212,7 +212,6 @@ public class RequestHandler {
             Log.d("Y:" + this.getClass().getName(), "Couldn't handle NodeMessageRequest correctly !");
         }*/
 
-
         mController.sendToServer(serverRequest);
     }
 
