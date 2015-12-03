6f262670c1a76b1bf232e139d7b6fb5822913f66
DelamareA <arnaud.delamare@epfl.ch>
2015-12-03 15:39:25 +0100

    Add basic implementation of rss creation on client

---
+++ client/app/src/main/java/yields/client/activities/CreateRSSFeedActivity.java
@@ -0,0 +1,15 @@
+package yields.client.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import yields.client.R;
+
+public class CreateRSSFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_rssfeed);
+    }
+}
