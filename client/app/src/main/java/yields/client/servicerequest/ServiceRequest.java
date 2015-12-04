de18e2cbb02dfd7ceff557d2b6f6f2fa426a87eb
Trofleb <ncasademont@bluewin.ch>
2015-12-04 09:14:47 +0100

    Change name for server request

--- client/app/src/main/java/yields/client/servicerequest/ServiceRequest.java
+++ client/app/src/main/java/yields/client/servicerequest/ServiceRequest.java
@@ -12,7 +12,7 @@ public abstract class ServiceRequest {
      */
     public enum RequestKind {
         PING("PING"), USER_CONNECT("UserConnect"), USER_UPDATE("UserUpdate"), USER_SEARCH("UserSearch"),
-        USER_GROUP_LIST("UserGroupList"), USER_ENTOURAGE_ADD("UserEntourageAdd"),
+        USER_GROUP_LIST("UserNodeList"), USER_ENTOURAGE_ADD("UserEntourageAdd"),
         USER_ENTOURAGE_REMOVE("UserEntourageRemove"), USER_INFO("UserInfo"),
         GROUP_CREATE("GroupCreate"), GROUP_UPDATE("GroupUpdate"), GROUP_UPDATE_NAME("GroupUpdateName"),
         GROUP_UPDATE_VISIBILITY("GroupUpdateVisibility"), GROUP_UPDATE_IMAGE("GroupUpdateImage"),
