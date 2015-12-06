3c7118b8c62df63ca7f71268322b09ffe9713b9b
Teo Stocco <teo.stocco@epfl.ch>
2015-12-06 18:36:05 +0100

    improve tests with probe

--- server/src/test/scala/yields/server/router/ClientHubTests.scala
+++ server/src/test/scala/yields/server/router/ClientHubTests.scala
@@ -2,10 +2,9 @@ package yields.server.router
 
 import java.net.InetSocketAddress
 
-import akka.actor.ActorSystem
 import akka.io.Tcp
 import akka.stream.actor.ActorSubscriberMessage
-import akka.testkit.TestActorRef
+import akka.testkit.{TestActorRef, TestProbe}
 import yields.server.actions.users.{UserConnectRes, UserSearchRes}
 import yields.server.io._
 import yields.server.mpi.{Metadata, Response}
@@ -20,34 +19,32 @@ class ClientHubTests extends YieldsAkkaSpec {
   import SerializationModule._
   import Tcp._
 
-  lazy val hub = {
-    val hubProps = ClientHub.props(self, InetSocketAddress.createUnresolved("", 0), self)
-    TestActorRef[ClientHub](hubProps)
-  }
-
   "ClientHub" should "refuse answering until user is connected" in {
+
+    val socket = TestProbe()
+    val dispatcher = TestProbe()
+    val hubProps = ClientHub.props(socket.ref, InetSocketAddress.createUnresolved("", 0), dispatcher.ref)
+    val hub = TestActorRef[ClientHub](hubProps)
 
     val searchResponse = serialize(Response(UserSearchRes(0), Metadata.now(0)))
     val connectResponse = serialize(Response(UserConnectRes(0, returning = true), Metadata.now(0)))
     val errorMessage = hub.underlyingActor.errorMessage
 
     hub ! OnNext(searchResponse)
-    expectMsg(Write(errorMessage, WriteAck(errorMessage)))
+    socket.expectMsg(Write(errorMessage, WriteAck(errorMessage)))
     hub ! WriteAck(errorMessage)
 
     hub ! OnNext(searchResponse)
-    expectMsg(Write(errorMessage, WriteAck(errorMessage)))
+    socket.expectMsg(Write(errorMessage, WriteAck(errorMessage)))
     hub ! WriteAck(errorMessage)
 
     hub ! OnNext(connectResponse)
-    expectMsgAllOf(
-      Write(connectResponse, WriteAck(connectResponse)),
-      InitConnection(0)
-    )
+    socket.expectMsg(Write(connectResponse, WriteAck(connectResponse)))
+    dispatcher.expectMsg(InitConnection(0))
     hub ! WriteAck(connectResponse)
 
     hub ! OnNext(searchResponse)
-    expectMsg(Write(searchResponse, WriteAck(searchResponse)))
+    socket.expectMsg(Write(searchResponse, WriteAck(searchResponse)))
 
   }
 
