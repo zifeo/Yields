43408ef691e63275a28db29353c5b5bf2db44a0c
Teo Stocco <teo.stocco@epfl.ch>
2015-12-08 10:28:54 +0100

    add receivers for forwarding message further

--- server/src/main/scala/yields/server/dbi/models/Publisher.scala
+++ server/src/main/scala/yields/server/dbi/models/Publisher.scala
@@ -32,7 +32,7 @@ final class Publisher private(nid: NID) extends Node(nid) with Tags {
   /** Add message */
   override def addMessage(content: FeedContent): Boolean = {
     val parent = super.addMessage(content)
-    val children = for (node <- nodes if parent) yield {
+    val children = for (node <- receivers if parent) yield {
       Node(node).addMessage(content.copy(_2 = nid))
     }
     children.foldLeft(parent)(_ && _)
