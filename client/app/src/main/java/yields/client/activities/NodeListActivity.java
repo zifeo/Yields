b2ea10c59f04c6a1ffd5cdf3a734fe79f602290d
DelamareA <arnaud.delamare@epfl.ch>
2015-12-08 10:46:09 +0100

    Creation of NodeListActivity

---
+++ client/app/src/main/java/yields/client/activities/NodeListActivity.java
@@ -0,0 +1,15 @@
+package yields.client.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import yields.client.R;
+
+public class NodeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_node_list);
+    }
+}
