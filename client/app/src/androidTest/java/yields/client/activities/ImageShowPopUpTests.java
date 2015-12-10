164ec026ba18b6740a4fa7dae4c7af6d556c8784
Justinien Bouron <justinien.bouron@epfl.ch>
2015-12-10 11:27:19 +0100

    ImageShowPopUpTests : Remove unused imports.

--- client/app/src/androidTest/java/yields/client/activities/ImageShowPopUpTests.java
+++ client/app/src/androidTest/java/yields/client/activities/ImageShowPopUpTests.java
@@ -1,9 +1,6 @@
 package yields.client.activities;
 
-import android.app.Activity;
-import android.app.Fragment;
 import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
@@ -11,45 +8,17 @@ import android.os.SystemClock;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
-import android.util.Log;
 import android.view.Display;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
-
 import yields.client.R;
 import yields.client.generalhelpers.ServiceTestConnection;
-import yields.client.id.Id;
-import yields.client.messages.CommentView;
-import yields.client.messages.ImageContent;
-import yields.client.messages.Message;
-import yields.client.messages.MessageView;
-import yields.client.messages.TextContent;
-import yields.client.node.ClientUser;
-import yields.client.node.Group;
 import yields.client.yieldsapplication.YieldsApplication;
-
-import static android.support.test.espresso.Espresso.closeSoftKeyboard;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.actionWithAssertions;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withTagValue;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.is;
-
 /**
  * Tests which test the MessageActivity (display and interaction).
  */
