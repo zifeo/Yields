cf28cbbdea449a0824cb35129cbddc9bbebefff8
Trofleb <ncasademont@bluewin.ch>
2015-12-11 13:40:31 +0100

    Repair publisher messages

--- client/app/src/main/java/yields/client/service/RequestHandler.java
+++ client/app/src/main/java/yields/client/service/RequestHandler.java
@@ -265,7 +265,6 @@ public class RequestHandler {
      * Handles the appropriate ServiceRequest which is given to it by argument.
      */
     protected void handleNodeInfoRequest(ServiceRequest serviceRequest) {
-        Log.d("JFBADFIASIFAS", "kabdjfafgi");
         ServerRequest serverRequest = serviceRequest.parseRequestForServer();
         mController.sendToServer(serverRequest);
     }
